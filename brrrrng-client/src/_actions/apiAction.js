9ce1aadcd6fdf204ac3af47ed8559141945f725c
segyong <segyong56@gmail.com>
2021-11-02 20:08:31 +0900

    refact:리팩토링,env파일추가

--- brrrrng-client/src/_actions/apiAction.js
+++ brrrrng-client/src/_actions/apiAction.js
@@ -1,11 +1,11 @@
 import { GET_STATIONS } from './types'
 import axios from 'axios'
 
-const API_URL = "https://api.brrrrng.ga"
+// const API_URL = "https://api.brrrrng.ga"
 
 export const getStations = (data) => {
   
-   const request = axios.post(`${API_URL}/charge/search`, data).then(response => response.data.response.body.items.item)
+   const request = axios.post(`${process.env.REACT_APP_API_URL}/charge/search`, data).then(response => response.data.response.body.items.item)
 
    return {
      type: GET_STATIONS,
